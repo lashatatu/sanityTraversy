06ae9308f72d92c78355c6e0bdace224b59b34da
lasha tatulashvili <lashatatu@gmail.com>
2020-12-09 19:05:30 +0400

    project

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,6 +6,7 @@ export default function Home(){
         <main>
             <img
                 src={image}
+                alt="Monstera Leaves"
                  className={'absolute object-cover w-full h-full'}/>
             <section className={'relative flex justify-center min-hscreen pt-12 lg:pt-64 px-8'}>
                 <h1 className={'text6-xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name'}>Hi. I'am Lasha</h1>
